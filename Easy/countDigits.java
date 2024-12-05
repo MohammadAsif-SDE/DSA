b8372f505d3cc483a1a5f82f90acea69e95e9e79
Mohammad Asif <mohammadaasif078p@gmail.com>
2024-12-05 16:07:13 +0530

    Practicing other set of progrems

---
+++ Easy/countDigits.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class countDigits {
+    static int digitcnt(int n){
+        int cnt = 0;
+        int originalNumber = n;
+
+        while (n>0){
+            int digit = n%10;
+            if(digit !=0 && originalNumber % digit == 0){
+                cnt++;
+            }
+            n/=10;
+        }
+        return cnt;
+    }
+    public static void main(String[] args){
+        System.out.print("Enter a Number to Count: ");
+        Scanner Sc = new Scanner(System.in);
+        int n = Sc.nextInt();
+
+        int result = digitcnt(n);
+        System.out.println("Number of digits that divide " + n + ": " + result);
+    }
+}
